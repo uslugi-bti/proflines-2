f988cad4cd0d68fd8ac9779a12c1ec06f89515ef
Evgeniy Zhuranskiy <evgenijzuranskij72@gmail.com>
2026-01-28 16:59:34 +0200

    auto-commit

--- #src/js/script.js
+++ #src/js/script.js
@@ -769,4 +769,54 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     new HeroAnimation();
+
+    if (document.querySelector(".expertise-nums")) {
+        const blockTitles = document.querySelectorAll(".expertise-nums-item__title span");
+        const blockColumns = document.querySelectorAll(".expertise-nums__item");
+        const arrTexts = [];
+        const animationDone = [];
+
+        for (let i = 0; i < blockTitles.length; i++) {
+            var blockTitlesText = Number(blockTitles[i].id);
+            arrTexts.push(blockTitlesText);
+            animationDone.push(false);
+        }
+
+        var animateCounter = function (element, endValue) {
+            let startValue = 0;
+            let duration = 1500;
+            let startTime;
+
+            function step(timestamp) {
+                if (!startTime) startTime = timestamp;
+                const progress = (timestamp - startTime) / duration;
+
+                if (progress < 1) {
+                    const value = Math.floor(startValue + (endValue - startValue) * progress);
+                    element.textContent = value;
+                    requestAnimationFrame(step);
+                } else {
+                    element.textContent = endValue;
+                }
+            }
+
+            requestAnimationFrame(step);
+        };
+
+        var increment = function () {
+            for (let i = 0; i < blockColumns.length; i++) {
+                var blockColumnTop = blockColumns[i].getBoundingClientRect().top;
+                var koef = 2;
+
+                if (blockColumnTop < window.innerHeight - (blockColumns[i].clientHeight / koef) && blockColumnTop > 0 && !animationDone[i]) {
+                    blockTitles[i].classList.add("active");
+                    animateCounter(blockTitles[i], arrTexts[i]);
+                    animationDone[i] = true;
+                }
+            }
+        }
+
+        window.addEventListener("load", increment);
+        window.addEventListener("scroll", increment);
+    }
 });
