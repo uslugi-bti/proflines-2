498f0871d02c54653099ac2ed53aff1492e9eddd
Evgeniy Zhuranskiy <evgenijzuranskij72@gmail.com>
2026-02-05 10:27:26 +0200

    auto-commit

--- #src/js/script.js
+++ #src/js/script.js
@@ -504,7 +504,7 @@ document.addEventListener("DOMContentLoaded", function () {
         window.addEventListener("resize", portfolioCoverWidth);
     }
 
-    /*if (document.querySelector(".technical")) {
+    if (document.querySelector(".technical")) {
         const listItems = document.querySelectorAll('.technical-list__item');
         const contentSections = document.querySelectorAll('.technical-content__body');
 
@@ -555,7 +555,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         window.addEventListener('scroll', handleScroll);
         handleScroll();
-    }*/
+    }
 
     const missionHeroBlocks = document.querySelectorAll('.mission-hero');
 
